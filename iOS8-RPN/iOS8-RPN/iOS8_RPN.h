8331dde5ff762c7882a0bfec5a6e584574303794
Paul Solt <PaulSolt@gmail.com>
2019-09-18 12:56:45 -0400

    Added starter projects for calculator

---
+++ iOS8-RPN/iOS8-RPN/iOS8_RPN.h
@@ -0,0 +1,19 @@
+//
+//  iOS8_RPN.h
+//  iOS8-RPN
+//
+//  Created by Paul Solt on 9/18/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS8_RPN.
+FOUNDATION_EXPORT double iOS8_RPNVersionNumber;
+
+//! Project version string for iOS8_RPN.
+FOUNDATION_EXPORT const unsigned char iOS8_RPNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS8_RPN/PublicHeader.h>
+
+
