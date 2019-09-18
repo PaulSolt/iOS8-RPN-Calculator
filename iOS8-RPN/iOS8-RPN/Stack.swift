3baa2ee3cfe03157b1eb1530388b400325ffb7e4
Paul Solt <PaulSolt@gmail.com>
2019-09-18 14:01:57 -0400

    Added unit tests for a Stack class that is generic

--- iOS8-RPN/iOS8-RPN/Stack.swift
+++ iOS8-RPN/iOS8-RPN/Stack.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+
+// Access Modifiers
+// - private
+// - internal
+// - public
+
+public struct Stack<Element>: ExpressibleByArrayLiteral {
+    
+    var items: [Element]
+    
+    public init(_ array: [Element] = []) {
+        self.items = array
+    }
+    
+    public init(arrayLiteral elements: Element...) {
+        self.items = elements
+    }
+
+    public mutating func push(_ value: Element) {
+        items.append(value)
+    }
+    
+    public mutating func pop() -> Element? {
+        guard items.count != 0 else { return nil }
+        return items.removeLast()
+    }
+    
+    public func peek() -> Element? {
+        return items.last
+    }
+    
+}
--- iOS8-RPN/iOS8-RPNTests/StackTests.swift
+++ iOS8-RPN/iOS8-RPNTests/StackTests.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import XCTest
+//@testable import iOS8_RPN
+import iOS8_RPN
 
 // For Frameworks don't use @testable, we can test public / internal / private better
 // as an API consumer
@@ -17,11 +19,26 @@ class StackTests: XCTestCase {
     func testStack() {
         
         // FILO: First In, Last Out data structure
-        //var stack: Stack<Int> = [1, 2, 3]  // ExpresssibleByArrayLiteral
+//        var stack = Stack<Int>([4, 5, 6])
         
-        // pop = pop item and return it
-        // peek = look at last item
+        
+        var stack: Stack<Int> = [1, 2, 3]  // ExpressibleByArrayLiteral
+        
         // push = add item
+        stack.push(5)
+
+        // pop = pop item and return it
+        XCTAssertEqual(5, stack.pop())
+        
+        // peek = look at last item
+        XCTAssertEqual(3, stack.peek())
+        
+        XCTAssertEqual(3, stack.pop())
+        XCTAssertEqual(2, stack.pop())
+        XCTAssertEqual(1, stack.pop())
+        XCTAssertEqual(nil, stack.pop())
+        
+        XCTAssertEqual(nil, stack.peek())
     }
     
     
