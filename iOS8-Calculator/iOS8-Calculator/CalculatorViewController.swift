cefb8646c2046cc218e6c4112b5fe988eaa72a46
Paul Solt <PaulSolt@gmail.com>
2019-09-18 13:12:29 -0400

    Updated the UI and added action/outlet stubs

---
+++ iOS8-Calculator/iOS8-Calculator/CalculatorViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CalculatorViewController.swift
+//  iOS8-Calculator
+//
+//  Created by Paul Solt on 9/18/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet var textField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func decimalButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func divideButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func multiplyButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func subtractButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func returnButtonTapped(_ sender: UIButton) {
+        
+    }
+}
